4cb11e830cf5a853255a7d28ca5f3830afc19b5e
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-10 13:44:30 -0300

    feat: added handle toggle sidebar to base layout

--- src/components/common/base-layout.tsx
+++ src/components/common/base-layout.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./sidebar";
 import { Footer } from "./footer";
 import * as S from "./styles";
 
 export const BaseLayout = () => {
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+
+  const handleToggleSidebar = () => {
+    setIsCollapsed((prev) => !prev);
+  };
+
   return (
     <S.LayoutContainer>
-      <Sidebar />
-      <S.ContentWrapper>
+      <Sidebar
+        isCollapsed={isCollapsed}
+        onToggleSidebar={handleToggleSidebar}
+      />
+      <S.ContentWrapper isCollapsed={isCollapsed}>
         <S.MainContent>
           <Outlet />
         </S.MainContent>
