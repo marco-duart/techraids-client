f00f64d7d89ddca3697245f9628972da14e54069
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-11 17:36:45 -0300

    feat: created private route

---
+++ src/components/private-route/index.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useAuth } from "../../context/user-provider";
+import { useNavigate } from "react-router-dom";
+import { IUser } from "../../services/auth/DTO";
+import { ReactNode } from "react";
+
+interface PrivateRouteProps {
+  requiredRole: IUser.Role.CHARACTER | IUser.Role.NARRATOR;
+  children?: ReactNode;
+}
+
+export const PrivateRoute = ({ requiredRole, children }: PrivateRouteProps) => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    } else if (user.role !== requiredRole) {
+      navigate("/access-denied");
+    }
+  }, [user, requiredRole, navigate]);
+
+  return user?.role === requiredRole ? <>{children}</> : null;
+};
