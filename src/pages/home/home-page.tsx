00b060f8ee671be674b526b37355d3e1e3f146fe
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-14 17:20:32 -0300

    feat: updated home page

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,9 +1,12 @@
 import * as S from "./styles";
+import { useCharacterQuest } from "../../hooks/use-character-quest";
 
 export const HomePage = () => {
+  const { data } = useCharacterQuest();
   return (
     <S.PageContainer>
       <h1>Home Page</h1>
+      <div>{data?.quest.title}</div>
     </S.PageContainer>
   );
 };
