a17aec45a0dcb110ee9e180fe554eb3fa35d3c32
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-14 17:17:58 -0300

    fear: added enum to mission dto

--- src/services/mission/DTO.ts
+++ src/services/mission/DTO.ts
@@ -1,9 +1,14 @@
 export namespace IMission {
+  export enum Status {
+    PENDING = "pending",
+    APPROVED = "approved",
+    CHARACTER = "rejected",
+  }
   export interface Model {
     id: number;
     title: string;
     description: string;
-    status: number;
+    status: Status;
     gold_reward: number;
     created_at: string;
     updated_at: string;
