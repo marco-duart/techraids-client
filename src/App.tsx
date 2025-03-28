e7a25fa2c456e440f8ba86d5cc3a1c32921d97bb
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-03-28 16:21:25 -0300

    feat: added toast config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import GlobalStyles from "./assets/styles/global-styles";
 import { ThemeProvider } from "./context/theme-provider";
 import Router from "./routes";
 import { UserProvider } from "./context/user-provider";
+import { Toaster } from "react-hot-toast";
+import { toastConfig } from "./config/toast-config";
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <ThemeProvider defaultThemeType="auth" defaultThemeMode="light">
         <GlobalStyles />
         <UserProvider>
+          <Toaster position="top-center" toastOptions={{ ...toastConfig }} />
           <Router />
         </UserProvider>
       </ThemeProvider>
---
+++ src/config/toast-config.ts
@@ -0,0 +1,12 @@
+export const toastConfig = {
+  position: "top-center" as const,
+  className: "medieval-toast",
+  duration: 4000,
+  style: {
+    border: "2px solid #8b5e3c",
+    padding: "16px",
+    background: "#f4e9d8",
+    color: "#3e2723",
+    fontFamily: '"MedievalSharp", cursive',
+  },
+};
