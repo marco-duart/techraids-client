2df8dfc4cffee98dbb9d45ba2ca6296a16383c8f
marco-duart <marcoaurelio_db@hotmail.com>
2025-04-21 12:07:58 -0300

    feat: added reward and chest dtos

---
+++ src/services/treasure_chest/DTO.ts
@@ -0,0 +1,11 @@
+export namespace ITreasureChest {
+  export interface Model {
+    id: number;
+    title: string;
+    value: number;
+    active: true;
+    created_at: string;
+    updated_at: string;
+    guild_id: number;
+  }
+}
