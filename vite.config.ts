b2d590f7f562458f5bc336264f434fd1f69105c6
marco-duart <marcoaurelio_db@hotmail.com>
2025-06-07 08:07:37 -0300

    feat: added vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-		cors: {
-			origin: '*',
-			methods: '*',
-			allowedHeaders: '*'
-		},
-		allowedHosts: true
-	}
+    host: '0.0.0.0',
+    port: 5173,
+    cors: {
+      origin: '*',
+      methods: '*',
+      allowedHeaders: '*'
+    }
+  }
 })
