8d66ede2ab452f2ec8dcead0b8ac3eda4d9e1040
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-06-17 17:52:27 -0300

    feat: updated mission modal

--- src/components/modals/mission-modal.tsx
+++ src/components/modals/mission-modal.tsx
@@ -2,14 +2,12 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { missionSchema, MissionFormData } from "../../schemas/mission-schema";
 import { IMission } from "../../services/mission/DTO";
-import { Scroll } from "@styled-icons/fa-solid";
+import { Scroll, Times } from "@styled-icons/fa-solid";
+import { Cancel, Save } from "@styled-icons/material";
 import { Sword } from "@styled-icons/remix-line";
-import { Times } from "@styled-icons/fa-solid";
 import { Modal } from "./modal";
 import { IconButton } from "../buttons/icon-button";
-import { useAuth } from "../../context/user-provider";
-import { useCharacters } from "../../hooks/useCharacters";
-import { useChapters } from "../../hooks/useChapters";
+import { useNarratorGuild } from "../../hooks/use-narrator-guild";
 import * as S from "./styles";
 
 interface Props {
@@ -27,9 +25,7 @@ export const MissionModal = ({
   onSubmit,
   isLoading,
 }: Props) => {
-  const { token } = useAuth();
-  const { characters } = useCharacters({ token });
-  const { chapters } = useChapters({ token });
+  const { guildMembers } = useNarratorGuild();
 
   const {
     register,
@@ -44,7 +40,6 @@ export const MissionModal = ({
       status: mission?.status || "pending",
       gold_reward: mission?.gold_reward || 0,
       character_id: mission?.character_id || undefined,
-      chapter_id: mission?.chapter_id || undefined,
     },
   });
 
@@ -118,32 +113,14 @@ export const MissionModal = ({
             disabled={!!mission}
           >
             <option value="">Selecione um personagem</option>
-            {characters?.map((character) => (
-              <option key={character.id} value={character.id}>
-                {character.name} ({character.nickname})
+            {guildMembers?.map((member) => (
+              <option key={member.id} value={member.id}>
+                {member.name} ({member.nickname})
               </option>
             ))}
           </S.Select>
           {errors.character_id && (
             <S.ErrorMessage>{errors.character_id.message}</S.ErrorMessage>
-          )}
-        </S.InputGroup>
-
-        <S.InputGroup>
-          <S.Label htmlFor="chapter_id">Capítulo</S.Label>
-          <S.Select
-            id="chapter_id"
-            {...register("chapter_id", { valueAsNumber: true })}
-          >
-            <option value="">Selecione um capítulo</option>
-            {chapters?.map((chapter) => (
-              <option key={chapter.id} value={chapter.id}>
-                {chapter.title}
-              </option>
-            ))}
-          </S.Select>
-          {errors.chapter_id && (
-            <S.ErrorMessage>{errors.chapter_id.message}</S.ErrorMessage>
           )}
         </S.InputGroup>
 
@@ -163,20 +140,17 @@ export const MissionModal = ({
 
         <S.FormFooter>
           <IconButton
-            $variant="outline"
-            type="button"
+            variant="default"
+            icon={Cancel}
             onClick={handleClose}
             disabled={isLoading}
-          >
-            Cancelar
-          </IconButton>
-          <IconButton $variant="primary" type="submit" disabled={isLoading}>
-            {isLoading
-              ? "Salvando..."
-              : mission
-              ? "Atualizar Missão"
-              : "Criar Missão"}
-          </IconButton>
+          />
+          <IconButton
+            variant="primary"
+            icon={Save}
+            type="submit"
+            disabled={isLoading}
+          />
         </S.FormFooter>
       </S.FormContainer>
     </Modal>
