e1bf6093d212edc00e6570ae85dff55a468c9f64
marco-duart <marcoaurelio_db@hotmail.com>
2025-06-22 15:27:15 -0300

    feat: added narrator treasure chest page

---
+++ src/pages/narrator/narrator-treasure-chest/index.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import * as S from "./styles";
+import { useTreasureChests } from "../../../hooks/use-treasure-chests";
+import { TreasureChestTable } from "../../../components/tables/treasure-chest-table";
+import { TreasureChestModal } from "../../../components/modals/treasure-chest-modal";
+import { IconButton } from "../../../components/buttons/icon-button";
+import { Plus } from "@styled-icons/boxicons-regular";
+import LoadingSpinner from "../../../components/loading-spinner";
+import { ITreasureChest } from "../../../services/treasure-chest/DTO";
+
+export const NarratorTreasureChestPage = () => {
+  const {
+    treasureChests,
+    isLoading,
+    createTreasureChest,
+    activateTreasureChest,
+    deactivateTreasureChest,
+  } = useTreasureChests();
+
+  const [viewingTreasureChest, setViewingTreasureChest] = useState<
+    ITreasureChest.Model | undefined
+  >();
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+
+  const handleCreate = async (data: { title: string; value: number }) => {
+    const treasureChestData = {
+      title: data.title,
+      value: data.value,
+      active: true,
+    };
+    await createTreasureChest({ treasure_chest: treasureChestData });
+    setIsCreateModalOpen(false);
+  };
+
+  const handleToggleStatus = async (id: number, activate: boolean) => {
+    if (activate) {
+      await activateTreasureChest(id);
+    } else {
+      await deactivateTreasureChest(id);
+    }
+  };
+
+  return (
+    <S.PageContainer>
+      <S.Header>
+        <S.Title>Premiações</S.Title>
+        <S.Actions>
+          <IconButton
+            variant="primary"
+            onClick={() => setIsCreateModalOpen(true)}
+            icon={Plus}
+          />
+        </S.Actions>
+      </S.Header>
+
+      {isLoading ? (
+        <S.LoadingWrapper>
+          <LoadingSpinner />
+        </S.LoadingWrapper>
+      ) : (
+        <TreasureChestTable
+          treasureChests={treasureChests}
+          onView={(chest) => setViewingTreasureChest(chest)}
+          onToggleStatus={handleToggleStatus}
+        />
+      )}
+
+      <TreasureChestModal
+        isOpen={isCreateModalOpen}
+        onClose={() => setIsCreateModalOpen(false)}
+        onSubmit={handleCreate}
+        onActivate={() => {}}
+        onDeactivate={() => {}}
+      />
+
+      <TreasureChestModal
+        isOpen={!!viewingTreasureChest}
+        onClose={() => setViewingTreasureChest(undefined)}
+        onSubmit={() => {}}
+        treasureChest={viewingTreasureChest}
+        onActivate={activateTreasureChest}
+        onDeactivate={deactivateTreasureChest}
+        readOnly
+      />
+    </S.PageContainer>
+  );
+};
---
+++ src/pages/narrator/narrator-treasure-chest/styles.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { DEVICE } from "../../../utils/constants";
+
+export const PageContainer = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  @media ${DEVICE.tablet} {
+    padding: 2rem;
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme.accent};
+  font-size: 1.5rem;
+  font-weight: 500;
+
+  @media ${DEVICE.tablet} {
+    font-size: 2rem;
+  }
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const LoadingWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 300px;
+`;
