da8562cba0741075b96947db88587c8e59a33b4f
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-09-09 17:34:32 -0300

    feat: added pending rewards and deliver reward services and dtos

--- src/services/narrator-guild/DTO.ts
+++ src/services/narrator-guild/DTO.ts
@@ -1,8 +1,29 @@
 import { IUser } from "../auth/DTO";
+import { IBoss } from "../boss/DTO";
+import { ICharacterTreasureChest } from "../character-treasure-chest/DTO";
 
 export namespace IGetGuildMembers {
   export type Params = {
     token: string;
   };
   export type Response = IUser.Model[];
+}
+
+export namespace IGetPendingRewards {
+  export type Params = {
+    token: string;
+  };
+  export type Response = {
+    pending_bosses: IBoss.Model[];
+    pending_chests: ICharacterTreasureChest.Model[];
+  };
+}
+
+export namespace IDeliverRewards {
+  export type Params = {
+    token: string;
+    boss_id?: number;
+    character_treasure_chest_id?: number;
+  };
+  export type Response = ICharacterTreasureChest.Model | IBoss.Model;
 }
--- src/services/narrator-guild/index.ts
+++ src/services/narrator-guild/index.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios";
 import api from "../api";
-import { IGetGuildMembers } from "./DTO";
+import { IGetGuildMembers, IGetPendingRewards, IDeliverRewards } from "./DTO";
 
 export const GetGuildMembers = async (params: IGetGuildMembers.Params) => {
   try {
@@ -16,6 +16,79 @@ export const GetGuildMembers = async (params: IGetGuildMembers.Params) => {
     return {
       success: true,
       message: "Colaboradores recuperados com sucesso!",
+      data: response.data,
+    };
+  } catch (error) {
+    if (isAxiosError(error)) {
+      console.error("Erro na requisição:", error);
+
+      return {
+        success: false,
+        message: error.message,
+        code: error.response?.status || 500,
+      };
+    }
+
+    console.error("Erro desconhecido:", error);
+
+    return {
+      success: false,
+      message: "Erro desconhecido!",
+      code: 500,
+    };
+  }
+};
+
+export const GetPendingRewards = async (params: IGetPendingRewards.Params) => {
+  try {
+    const { token } = params;
+
+    const response = await api.get<IGetPendingRewards.Response>(
+      "/narrators/pending_rewards",
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    return {
+      success: true,
+      message: "Recompensas pendentes recuperadas com sucesso!",
+      data: response.data,
+    };
+  } catch (error) {
+    if (isAxiosError(error)) {
+      console.error("Erro na requisição:", error);
+
+      return {
+        success: false,
+        message: error.message,
+        code: error.response?.status || 500,
+      };
+    }
+
+    console.error("Erro desconhecido:", error);
+
+    return {
+      success: false,
+      message: "Erro desconhecido!",
+      code: 500,
+    };
+  }
+};
+
+export const DeliverRewards = async (params: IDeliverRewards.Params) => {
+  try {
+    const { token, ...data } = params;
+
+    const response = await api.post<IDeliverRewards.Response>(
+      "/narrators/deliver_reward",
+      data,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+
+    return {
+      success: true,
+      message: "Recompensas pendentes recuperadas com sucesso!",
       data: response.data,
     };
   } catch (error) {
