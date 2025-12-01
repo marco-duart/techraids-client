fe2831701ee24dae58888e861412e049a90658dc
Marco Aurélio <marcoaurelio_db@hotmail.com>
2025-12-01 17:27:21 -0300

    feat: add narrator quest dto

--- src/services/narrator-guild/DTO.ts
+++ src/services/narrator-guild/DTO.ts
@@ -1,12 +1,33 @@
 import { IUser } from "../auth/DTO";
 import { IBoss } from "../boss/DTO";
+import { IChapter } from "../chapter/DTO";
+import { IGuildMember } from "../character-quest/DTO";
 import { ICharacterTreasureChest } from "../character-treasure-chest/DTO";
+import { IQuest } from "../quest/DTO";
 
 export namespace IGetGuildMembers {
   export type Params = {
     token: string;
   };
   export type Response = IUser.Model[];
+}
+
+export namespace IGetNarratorQuest {
+  export type ChapterWithCharactersAndBoss = IChapter.Model &
+    {
+      guild_members: IGuildMember.Model[];
+      boss: IBoss.Model & {
+        finishing_character: IGuildMember.FinishingCharacter | null;
+        team_can_defeat: boolean;
+      };
+    };
+  export type Params = {
+    token: string;
+  };
+  export type Response = {
+    quest: IQuest.Model;
+    chapters: ChapterWithCharactersAndBoss[];
+  };
 }
 
 export namespace IGetPendingRewards {
